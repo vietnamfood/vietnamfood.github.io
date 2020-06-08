b68e66799d85608bbee8ef4fc96ce8f2f2019f8d
Vaedang <dhuuloc8818@gmail.com>
2020-06-08 22:15:13 +0700

    #3: fix ui all pages

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Menu click
 $("li#main-btn").click(function(){
+	$("li#lunch-btn a").removeClass("active")
+	$("li#main-btn a").addClass("active")
+	$("li#dinner-btn a").removeClass("active")
+	$("li#drink-btn a").removeClass("active")
+
 	$("#food-1 div div img").attr("src", "./img/food/banhmichao/0.jpg")	
 	$("#food-1 div div div a").attr("href","./pages/banhmichao.html")
 	$("#food-1 .col-md-8 .card-body .card-title").text("Bánh mì chảo")
@@ -8,6 +13,11 @@ $("li#main-btn").click(function(){
 
 
 $("li#lunch-btn").click(function(){
+	$("li#lunch-btn a").addClass("active")
+	$("li#main-btn a").removeClass("active")
+	$("li#dinner-btn a").removeClass("active")
+	$("li#drink-btn a").removeClass("active")
+
 	$("#food-1 div div img").attr("src", "./img/food/canhchuacaithao.jpg")	
 	$("#food-1 div div div a").attr("href","./pages/canhchuacaithao.html")
 	$("#food-1 .col-md-8 .card-body .card-title").text("Canh chua cải thảo")
@@ -15,10 +25,16 @@ $("li#lunch-btn").click(function(){
 
 
 $("li#dinner-btn").click(function(){
-	alert("Dinner")
+	$("li#lunch-btn a").removeClass("active")
+	$("li#main-btn a").removeClass("active")
+	$("li#dinner-btn a").addClass("active")
+	$("li#drink-btn a").removeClass("active")
 })
 
 
 $("li#drink-btn").click(function(){
-	alert("drink")
+	$("li#lunch-btn a").removeClass("active")
+	$("li#main-btn a").removeClass("active")
+	$("li#dinner-btn a").removeClass("active")
+	$("li#drink-btn a").addClass("active")
 })
