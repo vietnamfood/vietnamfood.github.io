d6cf8c877dc78b6800ccbb9e2e4e9060deddaa12
witcher-creator <dhuuloc8818@gmail.com>
2020-06-08 10:48:55 +0700

    update something, add main.js and using jquery

---
+++ main.js
@@ -0,0 +1,14 @@
+// Menu click
+$("li#main-btn").click(function(){
+
+	$("#food-1-img").attr("src", "./img/food/banhmichao/0.jpg")
+})
+$("li#lunch-btn").click(function(){
+	alert("Lunch")
+})
+$("li#dinner-btn").click(function(){
+	alert("Dinner")
+})
+$("li#drink-btn").click(function(){
+	alert("drink")
+})
