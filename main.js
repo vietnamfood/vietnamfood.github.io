92cbc19dabc90c9f7ba295c71203bcf9d81923e1
witcher-yen <myen0805@gmail.com>
2020-06-12 15:45:14 +0700

    #2: drink

--- main.js
+++ main.js
@@ -85,7 +85,65 @@ $("li#drink-btn").click(function(){
 	loadContainer();
 
 	//food attr
+	$("#food-1 div div img").attr("src", "./img/food/caphetrung/0.jpg")	
+	$("#food-1 div div div a").attr("href","./pages/caphetrung.html")
+	$("#food-1 .col-md-8 .card-body .card-title").text("Cà phê trứng")
+	$("#food-1 .col-md-8 .card-body .describle").text("Cafe trứng là món thức uống đặc trưng của Hà Nội.")
+	$("#food-1 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
 	
+	$("#food-2 div div img").attr("src", "./img/food/cocktailcherryamaretto/0.jpg")	
+	$("#food-2 div div div a").attr("href","./pages/cocktailcherryamaretto.html")
+	$("#food-2 .col-md-8 .card-body .card-title").text("Cocktail Cherry Amaretto")
+	$("#food-2 .col-md-8 .card-body .describle").text("Cocktail với vị cherry chua chua, ngọt ngọt hòa cùng rượu Amaretto thơm lừng cực ngon và hấp dẫn rất dễ làm nên bạn hãy thử làm ngay nhé.")
+	$("#food-2 .col-md-8 .card-body .date .text-muted").text("Last updated 08/06/2020")
+
+	$("#food-3 div div img").attr("src", "./img/food/duadam/0.jpg")	
+	$("#food-3 div div div a").attr("href","./pages/duadam.html")
+	$("#food-3 .col-md-8 .card-body .card-title").text("Dừa dầm")
+	$("#food-3 .col-md-8 .card-body .describle").text("Dừa dầm Hải Phòng - món ăn vặt giải nhiệt cực hấp dẫn cho những tín đồ hảo ngọt.")
+	$("#food-3 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
+
+	$("#food-4 div div img").attr("src", "./img/food/milodalgona/0.jpeg")	
+	$("#food-4 div div div a").attr("href","./pages/milodalgona.html")
+	$("#food-4 .col-md-8 .card-body .card-title").text("Milo dalgona")
+	$("#food-4 .col-md-8 .card-body .describle").text(" Dành cho những tín đồ không không thích vị đắng của cà phê.")
+	$("#food-4 .col-md-8 .card-body .date .text-muted").text("Last updated 08/06/2020")
+
+	$("#food-5 div div img").attr("src", "./img/food/nuocchanhsahatchia/0.jpg")	
+	$("#food-5 div div div a").attr("href","./pages/nuocchanhsahatchia.html")
+	$("#food-5 .col-md-8 .card-body .card-title").text("Nước chanh sả hạt chia")
+	$("#food-5 .col-md-8 .card-body .describle").text("Nước Chanh Hạt Chia Hương Sả là thức uống detox ngon lành lại đơn giản tại nhà.")
+	$("#food-5 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
+
+	$("#food-6 div div img").attr("src", "./img/food/suachuauong/0.jpg")	
+	$("#food-6 div div div a").attr("href","./pages/suachuauong.html")
+	$("#food-6 .col-md-8 .card-body .card-title").text("Sữa chua uống")
+	$("#food-6 .col-md-8 .card-body .describle").text("Ngon tuyệt cú mèo.")
+	$("#food-6 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
+
+	$("#food-7 div div img").attr("src", "./img/food/suadauphong/0.jpg")	
+	$("#food-7 div div div a").attr("href","./pages/suadauphong.html")
+	$("#food-7 .col-md-8 .card-body .card-title").text("Sữa đậu phộng")
+	$("#food-7 .col-md-8 .card-body .describle").text("Đậu phộng không chỉ được được chế biến thành nhiều món ăn hấp dẫn mà còn là thức uống rất tốt cho sức khỏe.")
+	$("#food-7 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
+
+	$("#food-8 div div img").attr("src", "./img/food/suagaoluc/0.jpg")	
+	$("#food-8 div div div a").attr("href","./pages/suagaoluc.html")
+	$("#food-8 .col-md-8 .card-body .card-title").text("Sữa gạo lức")
+	$("#food-8 .col-md-8 .card-body .describle").text("Sữa Gạo Lức là thức uống từ hạt vô cùng thơm ngon.")
+	$("#food-8 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
+
+	$("#food-9 div div img").attr("src", "./img/food/suatuoitranchauduongnau/0.jpg")	
+	$("#food-9 div div div a").attr("href","./pages/suatuopitranchauduongnau.html")
+	$("#food-9 .col-md-8 .card-body .card-title").text("Sữa tươi trân châu đường nâu")
+	$("#food-9 .col-md-8 .card-body .describle").text("Vị béo ngậy của sữa tươi thanh trùng cùng trân châu dai dai, ngọt ngọt - loại topping gây sốt.")
+	$("#food-9 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
+
+	$("#food-10 div div img").attr("src", "./img/food/tratranchauhoadaubiec/0.jpg")	
+	$("#food-10 div div div a").attr("href","./pages/tranchauhoadaubiec.html")
+	$("#food-10 .col-md-8 .card-body .card-title").text("Trân châu hoa đậu biếc")
+	$("#food-10 .col-md-8 .card-body .describle").text("Trà hoa Đậu Biếc hay còn được gọi với cái tên thân thương trà Lam Hồ Điệp với màu tim tím đặc trưng.")
+	$("#food-10 .col-md-8 .card-body .date .text-muted").text("Last updated 10/06/2020")
 })
 
 
